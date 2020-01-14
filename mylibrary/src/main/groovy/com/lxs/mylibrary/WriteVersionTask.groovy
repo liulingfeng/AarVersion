f9f04269ab97ad68a90a342a8a5cfab39574f7ea
liulingfeng <liulingfeng@mistong.com>
2020-01-14 10:25:12 +0800

    插件修改

--- mylibrary/src/main/groovy/com/lxs/mylibrary/WriteVersionTask.groovy
+++ mylibrary/src/main/groovy/com/lxs/mylibrary/WriteVersionTask.groovy
@@ -19,16 +19,29 @@ class WriteVersionTask extends DefaultTask {
         versionFile.eachLine { line ->
             if (line.contains("version =")) {
                 def buf = new StringBuffer(line)
-                defaultVersion = buf.substring(buf.indexOf("\"") + 1, buf.length() - 1).toString()
-                def versionCode = defaultVersion.replace(".", "").toInteger() + 1
-                int amt = versionCode / 100
-                int sw = versionCode % 100 / 10
-                int qw = versionCode % 10
-                buf.replace(buf.indexOf("version ="), buf.length(), "version = \"${amt}.${sw}.${qw}\"")
-                line = buf.toString()
+                defaultVersion = parseXml("/Users/liuxiaoshuai/Downloads/artifactory-oss-6.16.0/backup/backup-daily/current/repositories/gradle-dev-local/com/lxs/android/${project.name}/maven-metadata.xml")
+                if (defaultVersion != "") {
+                    def versionCode = defaultVersion.replace(".", "").toInteger() + 1
+                    int amt = versionCode / 100
+                    int sw = versionCode % 100 / 10
+                    int qw = versionCode % 10
+                    buf.replace(buf.indexOf("version ="), buf.length(), "version = \"${amt}.${sw}.${qw}\"")
+                    line = buf.toString()
+                }
             }
             updatedFileText.append(line + "\n")
         }
         versionFile.write(updatedFileText.toString())
+    }
+
+    static def parseXml(urlString) {
+        try {
+            def metaData = new XmlParser().parse(urlString)
+            def latestVersion = metaData.versioning.latest
+            return latestVersion.text()
+        } catch (Exception e) {
+            println "文件不存在${e.message}"
+        }
+        return ""
     }
 }
